5a1d7f4df3126e7de10057520fcef0b85f0195b3
Md Murad Hossain <mdmuradhossain33@gmail.com>
2020-03-23 12:57:55 -0700

    initial changes

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -1,7 +1,30 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Md Murad Hossain
- * Date: 3/23/2020
- * Time: 12:36 PM
- */
+@extends('layouts.app')
+
+@section('content')
+    <div class="card">
+        <div class="card-header">Create New Article</div>
+
+        <div class="card-body">
+            <form action="/post/store" method="post" enctype="multipart/form-data">
+                {{csrf_field()}}
+                <div class="form-group">
+                    <label for="title">Title</label>
+                    <input type="text" name="title" class="form-control"/>
+                </div>
+                <div class="form-group">
+                    <label for="image">Feature Image</label>
+                    <input type="file" name="featured_image" class="form-control"/>
+                </div>
+                <div class="form-group">
+                    <label for="content">Content</label>
+                    <textarea name="content" class="form-control" cols="5" rows="5"></textarea>
+                </div>
+                <div class="form-group">
+                    <div class="text-center">
+                        <button type="submit" class="btn btn-success">Publish</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+@endsection
